2c43ba1f8b1806dce26ec7e835ba7032da699f1d
Vaishnavi A B <vaichirao24@gmail.com>
2025-11-30 10:33:37 +0530

    chnge

--- add_reserve.php
+++ add_reserve.php
@@ -20,7 +20,6 @@
 		<li><a href = "aboutus.php">About us</a></li> |
 		<li><a href = "contactus.php">Contact us</a></li> |
 		<li><a href = "gallery.php">Gallery</a></li> |
-		<li><a href = "dineandlounge.php">Dine and Lounge</a></li> |			
 		<li><a href = "reservation.php">Make a reservation</a></li> |
 		<li><a href = "rulesandregulation.php">Rules and Regulation</a></li>
 	</ul>
